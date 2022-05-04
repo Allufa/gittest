e37fa10da7ea0b1b3f92dd180c73cd01f983cc28
Alexandr <allufa@gmail.com>
2022-05-04 15:52:14 +0500

    declaration/expression

---
+++ js/functions_7_function_definition_expression_declaration/main.js
@@ -0,0 +1,15 @@
+// function declaration
+
+function f1() {
+    console.log('function 1');
+}
+
+f1()
+
+//function expression
+
+const f3 = function() {
+    console.log('function f3');
+};
+
+f3();
